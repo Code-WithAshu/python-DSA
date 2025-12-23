9e33f95762a4ca98edfcc6ad37b8d40db04282b7
Code-WithAshu <ashwanthkb@gmail.com>
2025-12-23 20:09:56 +0530

    Add: Insertion At End

---
+++ Insert_At_End.py
@@ -0,0 +1,10 @@
+# The Time Complexity of the Insert At End is O(1)
+
+def insert(b):
+    a.append(None)
+    a[-1]=b
+
+a=[1,2,3,4,5]
+b=int(input("enter the number to be inserted:" ))
+insert(b)
+print(a)
